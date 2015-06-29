40ac74b0cbbdd19d5fb6a2a2e10e145a4bd5bc5e
Mike Beyer <miketbeyer@gmail.com>
2015-06-28 22:47:24 -0500

    updated main for testing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/mikebeyer/clc-sdk/clc"
@@ -12,9 +13,14 @@ func main() {
 		panic(err)
 	}
 	client := clc.New(config)
-	resp, err := client.Auth()
+	resp, err := client.GetDatacenter("VA1")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(resp)
+	b, err := json.MarshalIndent(resp, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", b[:])
+
 }
