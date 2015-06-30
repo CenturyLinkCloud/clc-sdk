8d7dc03af0a57c4386ff05164bb0b67e751c28cb
Mike Beyer <miketbeyer@gmail.com>
2015-06-29 22:41:02 -0500

    added http tests for successful http auth

--- clc/client.go
+++ clc/client.go
@@ -1,21 +1,67 @@
 package clc
 
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
 type Config struct {
-	Username string
-	Password string
-	Alias    string
+	User    User
+	Alias   string
+	BaseURL string
 }
 
 type Client struct {
-	config Config
+	config  Config
+	client  *http.Client
+	baseURL string
 }
 
 func New(config Config) *Client {
+	url := config.BaseURL
+	if url == "" {
+		url = "https://api.ctl.io/v2"
+	}
 	return &Client{
-		config: config,
+		config:  config,
+		client:  http.DefaultClient,
+		baseURL: url,
 	}
 }
 
 func (c *Client) Auth() (string, error) {
-	return "", nil
+	url := fmt.Sprintf("%s/authentication/login", c.baseURL)
+	b, err := json.Marshal(&c.config.User)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.Post(url, "application/json", ioutil.NopCloser(bytes.NewReader(b)))
+	if err != nil {
+		return "", err
+	}
+
+	auth := &Auth{}
+	if err := json.NewDecoder(resp.Body).Decode(auth); err != nil {
+		return "", err
+	}
+
+	return auth.Token, nil
+}
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Auth struct {
+	Username string   `json:"userName"`
+	Alias    string   `json:"accountAlias"`
+	Location string   `json:"locationAlias"`
+	Roles    []string `json:"roles"`
+	Token    string   `json:"bearerToken"`
 }
--- clc/client_test.go
+++ clc/client_test.go
@@ -1,6 +1,9 @@
 package clc_test
 
 import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/mikebeyer/clc-sdk/clc"
@@ -12,14 +15,61 @@ func TestInitializeClient(t *testing.T) {
 
 	client := clc.New(clc.Config{})
 
-	assert.NotNil(client, "expected client to not be nil")
+	assert.NotNil(client)
 }
 
 func TestAuth(t *testing.T) {
 	assert := assert.New(t)
 
-	client := clc.New(clc.Config{})
-	_, err := client.Auth()
+	expectedToken := "TOKEN"
+	resource := authResource(assert, expectedToken)
+	ms := mockServer(resource)
+	defer ms.Close()
 
-	assert.Nil(err, "no error expected during auth")
+	client := client(ms.URL)
+	token, err := client.Auth()
+
+	assert.Nil(err)
+	assert.Equal(expectedToken, token)
+}
+
+func authResource(assert *assert.Assertions, token string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			assert.Fail("Authentication method should be POST", r.Method)
+		}
+
+		if r.URL.Path != "/authentication/login" {
+			assert.Fail("Authentication hitting wrong endpoint", r.URL.Path)
+		}
+
+		user := &clc.User{}
+		if err := json.NewDecoder(r.Body).Decode(user); err != nil {
+			assert.Fail("Decoding user failed.")
+		}
+
+		auth := &clc.Auth{
+			Username: user.Username,
+			Alias:    "test",
+			Location: "VA1",
+			Roles:    []string{"AccountAdmin"},
+			Token:    token,
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(auth)
+	}
+}
+
+func mockServer(resource func(w http.ResponseWriter, r *http.Request)) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(resource))
+}
+
+func client(url string) *clc.Client {
+	config := clc.Config{
+		User:    clc.User{Username: "test.user", Password: "password"},
+		BaseURL: url,
+		Alias:   "test",
+	}
+	return clc.New(config)
 }
