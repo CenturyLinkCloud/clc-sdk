41ea192a1878cd4ea72df37ead095abf1f0c2260
Mike Beyer <miketbeyer@gmail.com>
2015-07-07 09:25:47 -0500

    added testing for envconfig

--- clc/client_test.go
+++ clc/client_test.go
@@ -3,6 +3,7 @@ package clc_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/mikebeyer/clc-sdk/clc"
@@ -19,8 +20,26 @@ func TestInitializeClient(t *testing.T) {
 	assert.NotNil(client.Status)
 }
 
+func TestEnvConfig(t *testing.T) {
+	assert := assert.New(t)
+
+	checkEnvVar("CLC_USERNAME")
+	checkEnvVar("CLC_PASSWORD")
+	checkEnvVar("CLC_ALIAS")
+
+	config := clc.EnvConfig()
+
+	assert.Equal(config.BaseURL, "https://api.ctl.io/v2")
+}
+
 func mockServer(resource func(w http.ResponseWriter, r *http.Request)) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(resource))
+}
+
+func checkEnvVar(name string) {
+	if os.Getenv(name) == "" {
+		os.Setenv(name, "abcd")
+	}
 }
 
 func client(url string) *clc.Client {
