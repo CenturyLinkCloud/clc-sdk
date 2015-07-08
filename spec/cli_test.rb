b7bf2e343c15d3b29ac0054d6d77b3e70a8972f6
Mike Beyer <miketbeyer@gmail.com>
2015-07-08 16:08:31 -0500

    update uats to steamline test

--- spec/cli_test.rb
+++ spec/cli_test.rb
@@ -3,46 +3,59 @@ require 'singleton'
 
 describe 'clc cli' do
   before :all do
-    `godep go build -o spec/cli ./cli`
-    @server = Server.instance
+    Cli.build
   end
 
-  it 'creates a server' do
-    name = 'sample'
-    json = JSON.parse(`./spec/cli server create -n #{name} -c 1 -m 1 -t standard -g 8aa8153a1ba24542908155da468bb71a -s UBUNTU-14-64-TEMPLATE`)
+  it 'creates, fetches and deletes a server' do
+    server = Cli.create('sample')
 
-    @server.uuid = get_id 'self', json
-    id = get_id 'status', json
+    expect(server.status).to eq('succeeded')
+
+    json = Cli.get(server.uuid)
+
+    expect(json['groupId']).to eq('8aa8153a1ba24542908155da468bb71a')
+    expect(json['details']['cpu']).to eq(1)
+    expect(json['details']['memoryMB']).to eq(1024)
+
+    delete = Cli.delete(json['id'])
+
+    expect(delete['server']).to eq(json['id'])
+  end
+end
+
+class Server
+  attr_accessor :name, :uuid, :status
+
+  def initialize(uuid)
+    @uuid = uuid
+  end
+end
+
+
+class Cli
+  def self.build
+    `godep go build -o spec/cli ./cli`
+  end
+
+  def self.create(name)
+    json = JSON.parse(`./spec/cli server create -n #{name} -c 1 -m 1 -t standard -g 8aa8153a1ba24542908155da468bb71a -s UBUNTU-14-64-TEMPLATE`)
+    id = json['links'].select{ |val| val['rel'] == 'status' }.flat_map{ |val| val['id'] }[0]
+    server = Server.new(id)
 
     status = JSON.parse(`./spec/cli status get #{id}`)['status']
     until status == 'succeeded' || status == 'failed' do
       status = JSON.parse(`./spec/cli status get #{id}`)['status']
       sleep 20
     end
-    expect(status).to eq('succeeded')
+    server.status = status
+    server
   end
 
-  def get_id(rel, json)
-    json['links'].select{ |val| val['rel'] == rel }.flat_map{ |val| val['id'] }[0]
+  def self.get(id)
+    JSON.parse(`./spec/cli server get #{id}`)
   end
 
-  it 'fetches a known server' do
-    json = JSON.parse(`./spec/cli server get #{@server.uuid}`)
-    @server.name = json['id']
-
-    expect(json['groupId']).to eq('8aa8153a1ba24542908155da468bb71a')
-    expect(json['details']['cpu']).to eq(1)
-    expect(json['details']['memoryMB']).to eq(1024)
+  def self.delete(name)
+    JSON.parse(`./spec/cli server delete #{@server.name}`)
   end
-
-  it 'deletes servers' do
-    json = JSON.parse(`./spec/cli server delete #{@server.name}`)
-
-    expect(json['server']).to eq(@server.name)
-  end
-end
-
-class Server
-  include Singleton
-  attr_accessor :name, :uuid
 end
