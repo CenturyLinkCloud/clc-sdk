e89a7c11a85a7e1dec3712f0cd44d83307030583
Mike Beyer <miketbeyer@gmail.com>
2015-07-12 21:23:46 -0500

    refactored client's http responsibility into new api package

---
+++ sdk/api/api.go
@@ -0,0 +1,146 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/mikebeyer/env"
+)
+
+func New(config Config) *Client {
+	return &Client{
+		Config: config,
+		client: http.DefaultClient,
+	}
+}
+
+type Client struct {
+	Config Config
+	Token  Token
+
+	client *http.Client
+}
+
+func (c *Client) Get(url string, resp interface{}) error {
+	return c.do("GET", url, nil, resp)
+}
+
+func (c *Client) Post(url string, body, resp interface{}) error {
+	b, err := c.serialize(body)
+	if err != nil {
+		return err
+	}
+	return c.do("POST", url, b, resp)
+}
+
+func (c *Client) Patch(url string, body, resp interface{}) error {
+	b, err := c.serialize(body)
+	if err != nil {
+		return err
+	}
+	return c.do("PATCH", url, b, resp)
+}
+
+func (c *Client) Delete(url string, resp interface{}) error {
+	return c.do("DELETE", url, nil, resp)
+}
+
+func (c *Client) serialize(body interface{}) (io.Reader, error) {
+	if body == nil {
+		return nil, nil
+	}
+
+	b := new(bytes.Buffer)
+	err := json.NewEncoder(b).Encode(body)
+	return b, err
+}
+
+func (c *Client) auth() (string, error) {
+	url := fmt.Sprintf("%s/authentication/login", c.Config.BaseURL)
+	b := new(bytes.Buffer)
+	err := json.NewEncoder(b).Encode(c.Config.User)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := http.Post(url, "application/json", b)
+	if err != nil {
+		return "", err
+	}
+
+	if resp.StatusCode >= 400 {
+		return "", fmt.Errorf("http err: [%s]", resp.Status)
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&c.Token); err != nil {
+		return "", err
+	}
+
+	return c.Token.Token, nil
+}
+
+func (c *Client) do(method, url string, body io.Reader, resp interface{}) error {
+	if !c.Token.Valid() {
+		token, err := c.auth()
+		if err != nil {
+			return err
+		}
+		c.Token.Token = token
+	}
+
+	req, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+c.Token.Token)
+
+	res, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode >= 400 {
+		return fmt.Errorf("http err: [%s]", res.Status)
+	}
+
+	return json.NewDecoder(res.Body).Decode(resp)
+}
+
+type Config struct {
+	User    User
+	Alias   string
+	BaseURL string
+}
+
+func EnvConfig() Config {
+	return Config{
+		User: User{
+			Username: env.MustString("CLC_USERNAME"),
+			Password: env.MustString("CLC_PASSWORD"),
+		},
+		Alias:   env.MustString("CLC_ALIAS"),
+		BaseURL: env.String("CLC_BASE_URL", "https://api.ctl.io/v2"),
+	}
+}
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Token struct {
+	Username string   `json:"userName"`
+	Alias    string   `json:"accountAlias"`
+	Location string   `json:"locationAlias"`
+	Roles    []string `json:"roles"`
+	Token    string   `json:"bearerToken"`
+}
+
+// TODO: Add some real validation logic
+func (t Token) Valid() bool {
+	return t.Token != ""
+}
