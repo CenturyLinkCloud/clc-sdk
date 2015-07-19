476e8d749ba51cec9b74db6e1381e19f6b80e50b
Mike Beyer <miketbeyer@gmail.com>
2015-07-18 21:00:00 -0500

    added support for create group

---
+++ sdk/group/group.go
@@ -0,0 +1,63 @@
+package group
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/mikebeyer/clc-sdk/sdk/api"
+)
+
+func New(client api.HTTP) *Service {
+	return &Service{
+		client: client,
+		config: client.Config(),
+	}
+}
+
+type Service struct {
+	client api.HTTP
+	config *api.Config
+}
+
+func (s *Service) Create(group Group) (*Response, error) {
+	resp := &Response{}
+	url := fmt.Sprintf("%s/groups/%s", s.config.BaseURL, s.config.Alias)
+	err := s.client.Post(url, group, resp)
+	return resp, err
+}
+
+type Response struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Locationid  string    `json:"locationId"`
+	Type        string    `json:"type"`
+	Status      string    `json:"status"`
+	Links       api.Links `json:"links"`
+	Groups      []Groups  `json:"groups"`
+	Changeinfo  struct {
+		Createddate  time.Time `json:"createdDate"`
+		Createdby    string    `json:"createdBy"`
+		Modifieddate time.Time `json:"modifiedDate"`
+		Modifiedby   string    `json:"modifiedBy"`
+	} `json:"changeInfo"`
+	Customfields []api.Customfields `json:"customFields"`
+}
+type Group struct {
+	Name          string             `json:"name"`
+	Description   string             `json:"description"`
+	Parentgroupid string             `json:"parentGroupId"`
+	Customfields  []api.Customfields `json:"customFields"`
+}
+
+type Groups struct {
+	ID           string    `json:"id"`
+	Name         string    `json:"name"`
+	Description  string    `json:"description"`
+	Locationid   string    `json:"locationId"`
+	Type         string    `json:"type"`
+	Status       string    `json:"status"`
+	Serverscount int       `json:"serversCount"`
+	Groups       []Groups  `json:"groups"`
+	Links        api.Links `json:"links"`
+}
---
+++ sdk/group/group_test.go
@@ -0,0 +1,77 @@
+package group_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/mikebeyer/clc-sdk/sdk/api"
+	"github.com/mikebeyer/clc-sdk/sdk/group"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCreateGroup(t *testing.T) {
+	assert := assert.New(t)
+
+	client := NewMockClient()
+	client.On("Post", "http://localhost/v2/groups/test", mock.Anything, mock.Anything).Return(nil)
+	service := group.New(client)
+
+	group := group.Group{
+		Name:          "new",
+		Description:   "my awesome group",
+		Parentgroupid: "12345",
+	}
+	resp, err := service.Create(group)
+
+	assert.Nil(err)
+	assert.Equal(group.Name, resp.Name)
+	assert.Equal(1, len(resp.Groups))
+	assert.Equal(group.Parentgroupid, resp.Groups[0].ID)
+	client.AssertExpectations(t)
+}
+
+func NewMockClient() *MockClient {
+	return &MockClient{}
+}
+
+type MockClient struct {
+	mock.Mock
+}
+
+func (m *MockClient) Get(url string, resp interface{}) error {
+	args := m.Called(url, resp)
+	return args.Error(0)
+}
+
+func (m *MockClient) Post(url string, body, resp interface{}) error {
+	json.Unmarshal([]byte(`{"id":"67890","name":"new","description":"my awesome group","locationId":"WA1","type":"default","status":"active","groups":[{"id":"12345","name":"Parent Group Name","description":"The parent group.","locationId":"WA1","type":"default","status":"active","serversCount":0,"groups":[],"links":[{"rel":"createGroup","href":"/v2/groups/acct","verbs":["POST"]},{"rel":"createServer","href":"/v2/servers/acct","verbs":["POST"]},{"rel":"self","href":"/v2/groups/acct/12345","verbs":["GET","PATCH","DELETE"]},{"rel":"parentGroup","href":"/v2/groups/acct/12345","id":"12345"},{"rel":"defaults","href":"/v2/groups/acct/12345/defaults","verbs":["GET","POST"]},{"rel":"billing","href":"/v2/groups/acct/12345/billing"},{"rel":"archiveGroupAction","href":"/v2/groups/acct/12345/archive"},{"rel":"statistics","href":"/v2/groups/acct/12345/statistics"},{"rel":"upcomingScheduledActivities","href":"/v2/groups/acct/12345/upcomingScheduledActivities"},{"rel":"horizontalAutoscalePolicyMapping","href":"/v2/groups/acct/12345/horizontalAutoscalePolicy","verbs":["GET","PUT","DELETE"]},{"rel":"scheduledActivities","href":"/v2/groups/acct/12345/scheduledActivities","verbs":["GET","POST"]}]}],"links":[{"rel":"self","href":"/v2/groups/acct/67890"},{"rel":"parentGroup","href":"/v2/groups/acct/12345","id":"12345"},{"rel":"billing","href":"/v2/groups/acct/67890/billing"},{"rel":"archiveGroupAction","href":"/v2/groups/acct/67890/archive"},{"rel":"statistics","href":"/v2/groups/acct/67890/statistics"},{"rel":"scheduledActivities","href":"/v2/groups/acct/67890/scheduledActivities"}],"changeInfo":{"createdDate":"2012-12-17T01:17:17Z","createdBy":"user@domain.com","modifiedDate":"2014-05-16T23:49:25Z","modifiedBy":"user@domain.com"},"customFields":[]}`), resp)
+	args := m.Called(url, body, resp)
+	return args.Error(0)
+}
+
+func (m *MockClient) Put(url string, body, resp interface{}) error {
+	args := m.Called(url, body, resp)
+	return args.Error(0)
+}
+
+func (m *MockClient) Patch(url string, body, resp interface{}) error {
+	args := m.Called(url, body, resp)
+	return args.Error(0)
+}
+
+func (m *MockClient) Delete(url string, resp interface{}) error {
+	args := m.Called(url, resp)
+	return args.Error(0)
+}
+
+func (m *MockClient) Config() *api.Config {
+	return &api.Config{
+		User: api.User{
+			Username: "test.user",
+			Password: "s0s3cur3",
+		},
+		Alias:   "test",
+		BaseURL: "http://localhost/v2",
+	}
+}
