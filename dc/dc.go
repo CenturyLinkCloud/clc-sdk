bf89bb0d19a97cf01fbdb3e8f57164cbbe5dd813
Albert Choi <albert.choi@gmail.com>
2015-10-09 12:08:36 -0700

    add DC capabilities

--- dc/dc.go
+++ dc/dc.go
@@ -32,8 +32,34 @@ func (s *Service) GetAll() ([]*Response, error) {
 	return dcs, err
 }
 
+func (s *Service) GetCapabilities(id string) (*CapabilitiesResponse, error) {
+	url := fmt.Sprintf("%s/datacenters/%s/%s/deploymentCapabilities", s.config.BaseURL, s.config.Alias, id)
+	c := &CapabilitiesResponse{}
+	err := s.client.Get(url, c)
+	return c, err
+}
+
 type Response struct {
 	ID    string    `json:"id"`
 	Name  string    `json:"name"`
 	Links api.Links `json:"links"`
+}
+
+type CapabilitiesResponse struct {
+	SupportsPremiumStorage     bool `json:"supportsPremiumStorage"`
+	SupportsBareMetalServers   bool `json:"supportsBareMetalServers"`
+	SupportsSharedLoadBalancer bool `json:"supportsSharedLoadBalancer"`
+	Templates                  []struct {
+		Name               string   `json:"name"`
+		Description        string   `json:"description"`
+		StorageSizeGB      string   `json:"storageSizeGB"`
+		Capabilities       []string `json:"capabilities"`
+		ReservedDrivePaths []string `json:"reservedDrivePaths"`
+	} `json:"templates"`
+	DeployableNetworks []struct {
+		Name      string `json:"name"`
+		NetworkId string `json:"networkId"`
+		Type      string `json:"type"`
+		AccountID string `json:"accountID"`
+	} `json:deployableNetworks`
 }
