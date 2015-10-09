75bcd65e51d852875c1fe42c7fbd5fd84034b1fa
Albert Choi <albert.choi@gmail.com>
2015-10-09 12:06:00 -0700

    allow any link to be fetched

--- api/link.go
+++ api/link.go
@@ -4,18 +4,26 @@ type Link struct {
 	Rel   string   `json:"rel,omitempty"`
 	Href  string   `json:"href,omitempty"`
 	ID    string   `json:"id,omitempty"`
+	Name  string   `json:"name,omitempty"`
 	Verbs []string `json:"verbs,omitempty"`
 }
 
 type Links []Link
 
 func (l Links) GetID(rel string) (bool, string) {
-	for _, v := range l {
-		if v.Rel == rel {
-			return true, v.ID
-		}
+	if ok, v := l.GetLink(rel); ok {
+		return true, (*v).ID
 	}
 	return false, ""
+}
+
+func (l Links) GetLink(rel string) (bool, *Link) {
+	for _, v := range l {
+		if v.Rel == rel {
+			return true, &v
+		}
+	}
+	return false, nil
 }
 
 type Customfields struct {
